26e54e1c505045180182d692e04d6ca43f00f426
Jim Wilson <jimw@splunk.com>
2023-06-17 09:43:42 -0600

    Add logs to ch5/toposort/main.go.

--- ch5/toposort/main.go
+++ ch5/toposort/main.go
@@ -9,9 +9,10 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
-//!+table
+// !+table
 // prereqs maps computer science courses to their prerequisites.
 var prereqs = map[string][]string{
 	"algorithms": {"data structures"},
@@ -34,7 +35,7 @@ var prereqs = map[string][]string{
 
 //!-table
 
-//!+main
+// !+main
 func main() {
 	for i, course := range topoSort(prereqs) {
 		fmt.Printf("%d:\t%s\n", i+1, course)
@@ -42,16 +43,31 @@ func main() {
 }
 
 func topoSort(m map[string][]string) []string {
+	fmt.Println("============================================================")
+	fmt.Println("topoSort: m=", m)
+	fmt.Println("============================================================\n")
 	var order []string
 	seen := make(map[string]bool)
 	var visitAll func(items []string)
 
+	spacesStr := "                              "
+	indentSpaces := 0
+
 	visitAll = func(items []string) {
+		indent := spacesStr[:indentSpaces]
+		fmt.Printf("%svisitAll: items=[%s]\n", indent, strings.Join(items, ", "))
 		for _, item := range items {
 			if !seen[item] {
+				indentSpaces += 2
+				fmt.Printf("%sitem '%s' not seen, calling visitAll([%s])\n\n", indent, item, strings.Join(m[item], ", "))
 				seen[item] = true
 				visitAll(m[item])
 				order = append(order, item)
+				fmt.Printf("%safter visitAll() item=%s:\n", indent, item)
+				fmt.Printf("%sorder=[%s]\n", indent, strings.Join(order, ", "))
+				indentSpaces -= 2
+			} else {
+				fmt.Printf("%sitem '%s' already seen\n", indent, item)
 			}
 		}
 	}
@@ -60,8 +76,15 @@ func topoSort(m map[string][]string) []string {
 	for key := range m {
 		keys = append(keys, key)
 	}
+	fmt.Println("============================================================")
+	fmt.Printf("keys=[%s]\n", strings.Join(keys, ", "))
+	fmt.Println("============================================================\n")
 
 	sort.Strings(keys)
+	fmt.Println("============================================================")
+	fmt.Printf("sorted keys=[%s]\n", strings.Join(keys, ", "))
+	fmt.Println("============================================================\n")
+
 	visitAll(keys)
 	return order
 }
